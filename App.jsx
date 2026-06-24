ae5db9acddc14ae0ccd2c84e494cb1e40dd0471d
ty06schnyder <ty06schnyder@gmail.com>
2026-06-24 11:48:29 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Pie } from "react-chartjs-2";
 import { ArcElement } from "chart.js";
 
-import { Bar } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2"; //test
 import {
   Chart as ChartJS,
   BarElement,

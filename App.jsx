7f1937df85691de1702f32489350c3871f2016a3
ty06schnyder <ty06schnyder@gmail.com>
2026-06-25 11:17:45 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"; //testing test
+import React, { useState } from "react"; //testt
 
 import { Pie } from "react-chartjs-2"; 
 import { ArcElement } from "chart.js";
